0c631ae5871fc1f56dbfdb5c6d64094e0145d605
Adriana <am20dipi@alum.siena.edu>
2021-03-25 19:31:49 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,6 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    # defining a show action that finds a particular post to make it available for display
   end
 end
